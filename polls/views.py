d667fdcc7656a0ab63de4dc2dc91412487b92102
jinsm2000 <jinsm2000@gmail.com>
2022-08-29 15:31:35 +0900

    first commit

---
+++ polls/views.py
@@ -0,0 +1,7 @@
+from curses.ascii import HT
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World")
+# Create your views here.
